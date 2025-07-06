652be3dfdc4f2d8195d52a1719d535883a46908e
Horizon-369 <kshitij.neymar10@gmail.com>
2025-07-06 20:52:21 +0530

    show feedback

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -5,9 +5,10 @@ import { Button } from "./ui/button";
 import Link from "next/link";
 import DisplayTechIcons from "./DisplayTechIcons";
 import { id } from "zod/v4/locales";
+import { getFeedbackByInterviewId } from "@/lib/actions/general.action";
 
-const InterviewCard = ({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
-    const feedback = null as Feedback | null;
+const InterviewCard = async ({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
+    const feedback = userId && id ? await getFeedbackByInterviewId({ interviewId: id, userId}): null;
     const normalixedType = /mix/gi.test(type) ? "Mixed" : type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format("MMM D, YYYY");
 
